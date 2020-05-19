8488cd075552cb5fb5d89b8f0f155073fe6f80b0
Gabriel Keeble-Gagnere <gk0a@science.depi.vic.gov.au>
2020-05-19 22:20:54 +1000

    Initial commit.

---
+++ creamcar.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+import sys
+
+def collapseLines(lines, currentBuild, collapsedLines):
+  if not lines: # End of the algorithm.
+    return collapsedLines + [currentBuild]
+  else:
+    # We can assume lines has an element.
+    head, *tail = lines # Head will be something, tail may be empty.
+    if not currentBuild: # Needed because can't unpack an empty list in python.
+      return collapseLines(tail,
+                           (head[9], [head]),
+                           collapsedLines)
+    else:
+      id, hits = currentBuild # Can do this because we know it isn't empty.
+      if head[9] != id:
+        return collapseLines(tail, 
+                             (head[9], hits + [head]),
+                             collapsedLines + [currentBuild])
+      else:
+        return collapseLines(tail,
+                             (head[9], hits + [head]),
+                             collapsedLines)
+
+def alignmentScore(alignmentLine):
+  identities = int(alignmentLine[0])
+  mismatches = int(alignmentLine[1])
+  ns = int(alignmentLine[3])
+  qGapCount = int(alignmentLine[4])
+  qGapBases = int(alignmentLine[5])
+  tGapCount = int(alignmentLine[6])
+  tGapBases = int(alignmentLine[7])
+  qLength = int(alignmentLine[10])
+  binaryString = "".join([str(int(bool(x))) for x in [identities + mismatches + ns + qGapCount != qLength,
+                                   tGapCount,
+                                   qGapCount,
+                                   ns,
+                                   mismatches]])
+  return int(binaryString,2)
+
+with open(sys.argv[1], "r") as fh:
+  lines = [x.strip().split() for x in fh.readlines()]
+  collapsed = collapseLines(lines[5:25], [], [])
+  for c in collapsed:
+    id, alignments = c
+    for a in alignments:
+      print(id, alignmentScore(a))
